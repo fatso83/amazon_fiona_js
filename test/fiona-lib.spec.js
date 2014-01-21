e4b13cbee4cbdf5686543b2c2b06d2cadd70df17
Carl-Erik Kopseng <carlerik@gmail.com>
2014-01-21 01:33:02 +0100

    First test with working code

--- test/fiona-lib.spec.js
+++ test/fiona-lib.spec.js
@@ -1,88 +1,123 @@
+var server
+  , xhr
+  , requests;
+
 describe("Fetching of personal documents", function() {
-    it("should do something", function() {
-        expect(1+1).to.be(2);
-    });
+
+  before(function () {  
+    xhr = sinon.useFakeXMLHttpRequest();
+    xhr.onCreate = function(xhr) {
+      requests.push(xhr);
+    }; 
+  });
+  beforeEach(function() { requests = []; });
+
+  it("should call an amazon uri", function() {
+    var callback = sinon.spy()
+      , AmazonFiona = {
+
+    /** 
+     * @param options.offset  default 0
+     * @param options.count default 15
+     */
+        personal_docs : function(options) {
+          var r = new XMLHttpRequest();
+          r.open("GET", "http://www.amazon.com/", true);
+          r.onload = function () {
+            // do something
+          };
+          r.send();
+        }
+      };
+
+      AmazonFiona.personal_docs();
+
+      expect(requests).to.have.length(1);
+      expect(requests[0].url).to.match(/amazon.com/);
+  });
+
+
 });
 
 /*
-    import requests, re, sys, json
-    from bs4 import BeautifulSoup
-    from html.parser import HTMLParser
+   import requests, re, sys, json
+   from bs4 import BeautifulSoup
+   from html.parser import HTMLParser
 
-    #The session object that persists cookies and default values across requests
-    s = requests.Session()
-    h = HTMLParser();
-    amazon = 'https://www.amazon.com'
+   #The session object that persists cookies and default values across requests
+   s = requests.Session()
+   h = HTMLParser();
+   amazon = 'https://www.amazon.com'
 
-    def usage():
-        print("USAGE:", sys.argv[0], " <email> <username> ")
-        sys.exit(1);
+   def usage():
+   print("USAGE:", sys.argv[0], " <email> <username> ")
+   sys.exit(1);
 
-    def essential_info_string(item):
-        return '[' + item['asin'] + ']  ' + h.unescape(item['title'])
+   def essential_info_string(item):
+   return '[' + item['asin'] + ']  ' + h.unescape(item['title'])
 
-    def fetch_bought_titles(offset=0, count=15):
-        return fetch('queryOwnership_refactored2.html', offset, count)
+   def fetch_bought_titles(offset=0, count=15):
+   return fetch('queryOwnership_refactored2.html', offset, count)
 
-    def fetch_personal_docs(offset=0, count=15):
-        return fetch('queryPdocs.html', offset, count)
+   def fetch_personal_docs(offset=0, count=15):
+   return fetch('queryPdocs.html', offset, count)
 
-    def delete_title(content_id):
-        r = s.post(amazon+'/gp/digital/fiona/du/fiona-delete.html', data = {'contentName':content_id})
+   def delete_title(content_id):
+   r = s.post(amazon+'/gp/digital/fiona/du/fiona-delete.html', data = {'contentName':content_id})
 
-    def fetch(action, offset, count):
-        r = s.post(amazon+'/gp/digital/fiona/manage/features/order-history/ajax/' + action,
-                data = {'offset':offset,'count':count} )
-        clean_json = remove_control_chars(r.text)
-        return json.loads(clean_json)['data']
+   def fetch(action, offset, count):
+   r = s.post(amazon+'/gp/digital/fiona/manage/features/order-history/ajax/' + action,
+   data = {'offset':offset,'count':count} )
+   clean_json = remove_control_chars(r.text)
+   return json.loads(clean_json)['data']
 
-    def remove_control_chars(json_string):
-        """remove control characters that are illegal json. Amazon does not remove them"""
-        return re.sub('[\x00-\x1f]', '',json_string)
+   def remove_control_chars(json_string):
+   """remove control characters that are illegal json. Amazon does not remove them"""
+   return re.sub('[\x00-\x1f]', '',json_string)
 
-    def init_session(email, password):
-        #we need to fake the user agent in order to get amazon to return anything other than 404
-        user_agent = "User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36"
-        extra_headers = { 'User-Agent' : user_agent }
-        s.headers.update(extra_headers)
+   def init_session(email, password):
+   #we need to fake the user agent in order to get amazon to return anything other than 404
+   user_agent = "User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36"
+   extra_headers = { 'User-Agent' : user_agent }
+   s.headers.update(extra_headers)
 
-        r = s.get(amazon + '/gp/digital/fiona/manage')
-        soup = BeautifulSoup(r.content)
-        inputs = soup.find_all('input')
+   r = s.get(amazon + '/gp/digital/fiona/manage')
+   soup = BeautifulSoup(r.content)
+   inputs = soup.find_all('input')
 
-        attributes = [i.attrs for i in inputs]
-        form_values = {}
+   attributes = [i.attrs for i in inputs]
+   form_values = {}
 
-        for a in attributes:
-            form_values[a.get('name')] = a.get('value')
-        form_values.pop(None)
+   for a in attributes:
+   form_values[a.get('name')] = a.get('value')
+   form_values.pop(None)
 
-        form_values['email'] = email
-        form_values['password'] = password
+   form_values['email'] = email
+   form_values['password'] = password
 
-        # This gets us in. The session cookies are crucial!
-        s.post( amazon + '/ap/signin',
-                data = form_values,
-                allow_redirects=True )
+   # This gets us in. The session cookies are crucial!
+   s.post( amazon + '/ap/signin',
+   data = form_values,
+   allow_redirects=True )
 
-    if __name__ == '__main__':
-        if len(sys.argv) < 3:
-            usage();
+   if __name__ == '__main__':
+   if len(sys.argv) < 3:
+   usage();
 
-        my_email = sys.argv[1]
-        my_password = sys.argv[2]
+   my_email = sys.argv[1]
+   my_password = sys.argv[2]
 
-        init_session(my_email, my_password)
-        docs = fetch_personal_docs()
-        books = fetch_bought_titles()
+   init_session(my_email, my_password)
+   docs = fetch_personal_docs()
+   books = fetch_bought_titles()
 
-        status_docs = "Has %s personal documents. The last one is %s"
-        status_books = "Has bought %s e-book titles from Amazon. The last one is %s"
-        print(status_docs % (docs['totalCount'], h.unescape(docs['items'][0]['title'])))
-        print(status_books % (books['totalCount'], h.unescape(books['items'][0]['title'])))
-        print("Last 15 personal documents:")
-        for doc in docs['items'][:15]:
-            print(essential_info_string(doc).encode('utf-8'))
+status_docs = "Has %s personal documents. The last one is %s"
+status_books = "Has bought %s e-book titles from Amazon. The last one is %s"
+print(status_docs % (docs['totalCount'], h.unescape(docs['items'][0]['title'])))
+print(status_books % (books['totalCount'], h.unescape(books['items'][0]['title'])))
+print("Last 15 personal documents:")
+for doc in docs['items'][:15]:
+print(essential_info_string(doc).encode('utf-8'))
 
 
 */
